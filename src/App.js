006543441b08b8f3bb36090e48f8f500b8f4400e
KoTaeHoon <koth1999@bizbee.co.kr>
2025-09-26 09:27:54 +0900

    20250926 커밋

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8081")
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message ? message : "테스트 연결123"}</p>
       </header>
     </div>
   );
